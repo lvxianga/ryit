75860c2b80e83e7c0dbcbd1655b9a12d28ecc03f
lvxiang <1035730889@qq.com>
2021-12-07 14:33:43 +0800

    vue

---
+++ vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <user_login></user_login>
+  </div>
+</template>
+
+<script>
+import user_login from './components/user_login'
+
+export default {
+  name: 'App',
+  components: {
+    user_login
+  }
+}
+</script>
+
+<style>
+
+</style>
