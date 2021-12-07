980d741a4a4dad5c4ca7388965658e9bae38c2ac
BoldPeanuts <86678691+BoldPeanuts@users.noreply.github.com>
2021-12-07 16:25:51 +0800

    Add files via upload

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,52 +1,70 @@
-package main.java.entity;
+package student.entity;
 
-import java.util.Objects;
-
+/**
+ * Description
+ * 该类用于表示用户，用于登录数据库的凭证
+ * @author BoldPeanuts
+ * @date Created on 2021/12/7
+ */
+@Table_Name(name = "user")
 public class User {
-
-    private String name;
+    @Column_Name(name = "user_name")
+    private String userName;
+    @Column_Name(name = "user_password")
     private String password;
-
-    public User(String name, String password) {
-        this.name = name;
+    private final String URL = "";
+    @Column_Name(name = "user_identity")
+    private String identity;
+    @Column_Name(name = "user_contact")
+    private String cotactInfo;
+    /**
+     *
+     * @param userName 用户名
+     * @param password 密码
+     */
+    public User(String userName, String password) {
+        this.userName = userName;
         this.password = password;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(name, user.name) &&
-                Objects.equals(password, user.password);
+    public User(){
+
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, password);
+    public String getUserName() {
+        return userName;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getName() {
-        return name;
+    public String getIdentity() {
+        return identity;
     }
 
-    public String getPassword() {
-        return password;
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    @Override
+    public String toString(){
+        return "用户名："+userName+"，密码："+password+"，身份："+identity;
+    }
+
+    public String getCotactInfo() {
+        return cotactInfo;
+    }
+
+    public void setCotactInfo(String cotactInfo) {
+        this.cotactInfo = cotactInfo;
     }
 }
