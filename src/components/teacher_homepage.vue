e8fb5d60f0eeceb5008c6d5154a4ae0eeaa98aeb
lvxiang <1035730889@qq.com>
2021-12-08 14:35:54 +0800

    vue1.0

---
+++ src/components/teacher_homepage.vue
@@ -0,0 +1,106 @@
+<template>
+    <ul class="shell">
+        <li class="button">
+            <span>作业模块</span>
+            <ul>
+                <li>布置作业</li>
+                <li>作业批改</li>
+                <li>发布分析报告</li>
+                <li>在线答疑</li>
+            </ul>
+        </li>
+        <li class="button">
+            <span>班级模块</span>
+            <ul>
+                <li>通告发布</li>
+                <li>小组管理</li>
+                <li>查看班级</li>
+                <li>联系家长</li>
+            </ul>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "teacher_homepage"
+    }
+</script>
+
+<style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        font-size: 20px;
+        color: #f4f1de;
+    }
+
+    body {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-image:linear-gradient(to right, #a18cd1, #fbc2eb);
+    }
+    .shell{
+        display: flex;
+        justify-content: center;
+    }
+    .button,.portrait{
+        background-color: #50536e;
+        width: 170px;
+        height: 65px;
+        text-align: center;
+        line-height: 65px;
+        transition: .3s;
+        cursor: pointer;
+    }
+    .portrait::before{
+        content: '';
+        display: block;
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        transform: translate(30px,-30px);
+        background: darkturquoise;
+        background-size: cover;
+        border: 10px solid #fff;
+    }
+    .information{
+        width: 220px;
+        background-color: #e07a5f;
+        transform: translate(-20px,-200px);
+        border-radius: 10px;
+        padding: 20px 0;
+        line-height: 0;
+        height: 0;
+        transition: .3s;
+        opacity: 0;
+
+    }
+    .button:hover{
+        background-color: #d15a39;
+    }
+    .button li:hover{
+        background-color: #ffb29d;
+    }
+    .button ul li{
+        height: 0;
+        transition: .2s;
+        opacity: 0;
+        transform: translateY(-65px);
+        background-color: #e07a5f;
+    }
+    .button:hover li{
+        height: 65px;
+        opacity: 1;
+        transform: translateY(0);
+    }
+    .portrait:hover .information{
+        opacity: 1;
+        line-height: 65px;
+        transform: translate(-20px,-10px);
+        height: auto;
+    }
+</style>
---
+++ src/components/user_login.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="a">
+        <div class="b"></div>
+        <div class="c">
+            <div class="d">
+                <form method="post" action="http://localhost:8080/web_war_exploded/userlogin?type=User">
+                    <h1>登录</h1>
+                    <input type="text" class="e" name="username" placeholder="用户名">
+                    <input type="text" class="e" name="password" placeholder="密码">
+                    <a href="#" class="f">忘记密码?</a>
+                    <input type="submit" class="g" value="登录"/>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "user_login"
+    }
+</script>
+
+<style scoped>
+    body{
+        /* 设置背景渐变 */
+        background-image: linear-gradient(to left,
+        #9c88ff,#3cadeb);
+        display: flex;
+        justify-content: center;
+    }
+    .a{
+        position:relative;
+        top: 100px;
+        left: 200px;
+        width: 1100px;
+        height: 550px;
+        box-shadow: 0 5px 15px rgba(0,0,0,.8);
+        display: flex;
+    }
+    .b{
+        width: 800px;
+        height: 550px;
+        background-image: url("../../src/img/user_backgourd.jpg");
+        /* 让图片适应大小 */
+        background-size: cover;
+    }
+    .c{
+        width: 300px;
+        height: 550px;
+        background-color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .d{
+        width: 250px;
+        height: 500px;
+    }
+    .d h1{
+        font: 900 30px '';
+    }
+    .e{
+        width: 230px;
+        margin: 20px 0;
+        outline: none;
+        border: 0;
+        padding: 10px;
+        border-bottom: 3px solid rgb(80,80,170);
+        font: 900 16px '';
+    }
+    .f{
+        float: right;
+        margin: 10px 0;
+    }
+    .g{
+        position: absolute;
+        margin: 20px;
+        bottom: 40px;
+        display: block;
+        width: 200px;
+        height: 60px;
+        font: 900 30px '';
+        text-decoration: none;
+        line-height: 50px;
+        border-radius: 30px;
+        background-image: linear-gradient(to left,
+        #9c88ff,#3cadeb);
+        text-align: center;
+    }
+</style>
