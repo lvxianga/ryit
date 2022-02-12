f0ce834895758543940618769033bd69acbafc7a
ryitedu <3207093479@qq.com>
2022-02-12 12:09:30 +0800

    1.1 "TeacherDaoImpl 添加了 run方法 输出一句 hello word

--- src/main/java/dao/teacherdao/teacherdaoimpl/TeacherDaoImpl.java
+++ src/main/java/dao/teacherdao/teacherdaoimpl/TeacherDaoImpl.java
@@ -4,6 +4,8 @@ import main.java.dao.teacherdao.TeacherDaoInter;
 
 public class TeacherDaoImpl implements TeacherDaoInter {
 
-
+        public void run(){
+            System.out.println("Hello Word");
+        }
 
 }
